814dcdbc7cc1a272b64e082b84d70dc94058e9b6
Lexfer Ramirez <lexferramirez@gmail.com>
2021-07-25 07:57:40 -0400

    add context with the theme provider

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,28 @@
  */
 
 // You can delete this file if you're not using it
+//gatsby-browser.js y gatsby-ssr.js lo usa gatby para el context de la app en gatsby
+//el mismo codigo de este archivo de estar en el archivo de gatsby-ssr.js
+import React from 'react'
+import {createGlobalStyle, ThemeProvider} from 'styled-components';
+import Theme from './src/themes/theme'
+
+const GlobalStyle = createGlobalStyle`
+    *{
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+    body,html{
+        font-family: ${props => props.theme.fonts.main};
+        height: 100%;
+        background-color: ${props => props.theme.colors.light1};
+    }
+`
+
+export const wrapRootElement = ({element}) =>{
+    <ThemeProvider theme={Theme}>
+        <GlobalStyle />
+        {element}
+    </ThemeProvider>
+}
